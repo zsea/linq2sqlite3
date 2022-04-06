ab3048796f13bccb8459ca41ebfe92b4d0f80174
曾大海 <zsea@qq.com>
2022-04-07 00:01:02 +0800

    修正lambda表达式查询

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ const Linq = require("../lib/");
 async function main() {
     //console.log("开始运行...")
     let db=await Linq.Open(null,null,function(sql,values){
-        console.log(sql,values);
+        //console.log(sql,values);
     });
     await db.execute(`CREATE TABLE IF NOT EXISTS  "t" (
         "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -10,7 +10,7 @@ async function main() {
       );`)
     let add=await db.table("t").insert({name:"曾大海ss"});
     console.log("写入结果",add);
-    let data=await db.table("t").toArray()
+    let data=await db.table("t").where(p=>p.id!=3).toArray()
     console.log("查询结果",data);
     process.exit();
 }
